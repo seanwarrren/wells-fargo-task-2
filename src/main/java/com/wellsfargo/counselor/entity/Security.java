04637d5eafbe034a32cdc94e5a73e6af00a3f66b
Sean Warren <seanwarren@Seans-MBP.socal.rr.com>
2024-07-20 01:26:51 -0700

    Implemented Client, Portfolio, and Security entities with JPA annotations

---
+++ src/main/java/com/wellsfargo/counselor/entity/Security.java
@@ -0,0 +1,96 @@
+package com.wellsfargo.counselor.entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.util.Date;
+
+@Entity
+public class Security {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int securityId;
+
+    @ManyToOne
+    @JoinColumn(name = "portfolioId", nullable = false)
+    private Portfolio portfolio;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private double purchasePrice;
+
+    @Column(nullable = false)
+    private Date purchaseDate;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    public Security(Portfolio portfolio, String name, String category, double purchasePrice, Date purchaseDate, int quantity) {
+        this.portfolio = portfolio;
+        this.name = name;
+        this.category = category;
+        this.purchasePrice = purchasePrice;
+        this.purchaseDate = purchaseDate;
+        this.quantity = quantity;
+    }
+
+    public int getSecurityId() {
+        return securityId;
+    }
+
+    public Portfolio getPortfolio() {
+        return portfolio;
+    }
+
+    public void setPortfolio(Portfolio portfolio) {
+        this.portfolio = portfolio;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(double purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public Date getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(Date purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
